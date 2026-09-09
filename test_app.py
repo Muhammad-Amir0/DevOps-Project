5de316e127bcd6b39fddef3d6536de4aa0dd93f0
Amir khan <amirkhan0211338@gmail.com>
2026-09-10 01:58:55 +0500

    Update test for Version 3

--- test_app.py
+++ test_app.py
@@ -13,7 +13,7 @@ def client():
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello from DevOps CI/CD Pipeline - Version2!"
+    assert response.data == b"Hello from DevOps CI/CD Pipeline - Version3!"
 
 
 def test_health(client):
